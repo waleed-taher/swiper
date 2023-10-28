5eb0d3f9aeeac4df8e4f52916e92752a42244406
Muhammad Waleed <waleed.taher17@gmail.com>
2023-10-28 11:25:49 +0500

    basic implementation of swiper js

---
+++ src/constants/mockData.ts
@@ -0,0 +1,18 @@
+export const dataDigitalBestSeller = [
+  {
+    id: 1,
+    title: "Mario Kart™ 8 Deluxe",
+    price: "$59.99",
+    category: "Nintendo Switch",
+    linkImg:
+      "https://images.unsplash.com/photo-1697830043651-8ead52ce5939?auto=format&fit=crop&q=80&w=1932&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 2,
+    title: "TRIANGLE STRATEGY™",
+    price: "$59.99",
+    category: "Nintendo Switch",
+    linkImg:
+      "https://media.istockphoto.com/id/92919522/photo/himalayan-sunrise-nepal.jpg?s=1024x1024&w=is&k=20&c=GLXRbtOIIWrMIzdR0HQ7Zy4KTAuOVGCvusX8mK9xyys=",
+  },
+];
